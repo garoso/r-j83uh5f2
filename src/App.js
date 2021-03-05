f0f4041c4749c1531eb766b42cf1055c7e82c7ba
Davste <david.fd.251@gmail.com>
2021-03-05 17:15:35 -0500

    Create textarea character counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      count: 0
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.counter.bind(this)}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
+  }
+  counter = (event) => {
+    this.setState({count: event.target.value.length})
   }
 }
 
